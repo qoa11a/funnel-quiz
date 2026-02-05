265d7c76f708d6c42caa00bff4014abc0f67e817
Ihor Honcharenko <goncharenkoid2001@gmail.com>
2026-02-05 23:13:24 +0200

    Disable Next.JS devtools

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   reactCompiler: true,
   typedRoutes: true,
+  devIndicators: false,
 };
 
 const withNextIntl = createNextIntlPlugin({
